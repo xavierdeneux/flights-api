b7931b0af325d4deea05a8c5df0c2fcba1963c1b
Xavier <x.deneux@gmail.com>
2019-02-26 17:00:39 +0100

    Commit project

--- index.js
+++ index.js
@@ -3,82 +3,94 @@ var app = express();
 
 const axios = require('axios');
 const geolib = require('geolib');
+const airports = require('airport-codes');
 
-var mock = 
-{"full_count":15134,"version":4,"1f9e0f95":["4BAA42",45.8425,5.0910,52,4050,216,"4065","F-LFLY1","A321","TC-JRB",1551115772,"LYS","IST","TK1810",0,896,"THY7JK",0,"THY"]
-,"stats":{"total":{"ads-b":11771,"mlat":1342,"faa":1039,"flarm":10,"estimated":703},"visible":{"ads-b":1,"mlat":0,"faa":0,"flarm":0,"estimated":0}}}
 
-var baseUrl = 'https://data-live.flightradar24.com';
+// Api flightradar24
+const baseUrl = 'https://data-live.flightradar24.com';
 
-var bigBounds = '46.04,45.53,4.43,5.76';
-var dagneuxBounds = '45.90,45.80,4.88,5.20';
-var dagneuxGPS_lt = '45.8551';
-var dagneuxGPS_lg = '5.0747';
+// Coordonnées GPS
+const bigBounds = '46.04,45.53,4.43,5.76'; // zone très large, utilisée pour débugguer l'app quand il n'y a pas assez de vol dans le périmètre
+const homeBounds = '46e.90,45.80,4.88,5.20';
+const homeLatitude = '45.8551';
+const homeLongitude = '5.0747';
 
-var bounds = bigBounds;
-//var bounds = dagneuxBounds;
+// On choisi le bounds qu'on va utiliser
+let bounds;
+bounds = bigBounds;
+//bounds = homeBounds;
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
-	//var distance = geolib.getDistance({latitude: dagneuxGPS_lt, longitude: dagneuxGPS_lg}, {latitude: "45.7880", longitude: "5.1085"});
+	const url = baseUrl+'/zones/fcgi/feed.js?bounds='+bounds+'&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1';
 
-	//console.log(distance);
-	
-	axios.get(baseUrl+'/zones/fcgi/feed.js?bounds='+bounds+'&faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=14400&gliders=1&stats=1').then(function(response) {
-		var flightResponse = response.data;
-		//var flightResponse = mock;
-		var flights = [];
-		var flightsDetail = {};
-		
-		for(let field in flightResponse){
-			if(['full_count','version','stats','visible'].indexOf(field) == -1){
-				flights.push(field);
+	axios.get(url).then(flightsInBoundsResponse => {
+		let flightsInBounds = flightsInBoundsResponse.data;
+		let flightsDetail = {};
+		let flightsPromises = [];
+
+		const excludedFields = ['full_count','version','stats','visible'];
+
+		for(let flight in flightsInBounds){
+			console.log('x', flightsInBounds)
+			// Si la propriété ne fait pas partie des champs exclus, c'est que c'est un avion.
+			if(excludedFields.indexOf(flight) == -1){
+				flightsDetail[flight] = {
+					latitude : flightsInBounds[flight][1],
+					longitude : flightsInBounds[flight][2],
+					altitude : flightsInBounds[flight][4] / 3.281, // On convertit à la volée pieds en mètres
+					speed : flightsInBounds[flight][5] * 1.60934, // On convertit à la volée miles/h en km/h
+				};
+
+				// Pour chaque vol dans la zone, on va devoir aller chercher le détail des ses infos.
+				flightsPromises.push(axios.get(baseUrl+'/clickhandler/?version=1.5&flight='+flight));
 			}
 		}
-		
-		var flightsPromises = [];
-		
-		for(let flight of flights){
-			flightsDetail[flight] = {};
-			flightsPromises.push(axios.get(baseUrl+'/clickhandler/?version=1.5&flight='+flight));
-				/*.then(function(response){
-				var data = response.data;
-				
-				flightsDetail[flight]['flight'] = data['identification']['callsign'];
-				flightsDetail[flight]['aircraft'] = data['aircraft']['model']['text'];
-				flightsDetail[flight]['airline'] = data['airline']['short'];
-				flightsDetail[flight]['origin'] = data['airport']['origin']['code']['iata'];
-				flightsDetail[flight]['destination'] = data['airport']['destination']['code']['iata'];
-				console.log('flightsDetail',flightsDetail);
-				res.send(flightsDetail);
-				console.log('res',flightsDetail);
-				
-			});*/
-		}
-		
-		//console.log('flightsPromises',flightsPromises);
-		Promise.all(flightsPromises).then(function(flightsResponse){
-			flightsResponse.forEach(flightResponse => {
-				var data = flightResponse.data;
-				//console.log('data',data);
-				var flight = data['identification']['id'];
-				flightsDetail[flight] = {};
-				flightsDetail[flight]['flight'] = data['identification']['callsign'];
-				flightsDetail[flight]['aircraft'] = data['aircraft']['model']['text'];
-				//flightsDetail[flight]['airline'] = data['airline']['short'];
-				//flightsDetail[flight]['origin'] = data['airport']['origin']['code']['iata'];
-				//flightsDetail[flight]['destination'] = data['airport']['destination']['code']['iata'];
-				flightsDetail[flight]['latitude'] = data['trail'][data['trail'].length-1]['lat'];
-				flightsDetail[flight]['longitude'] = data['trail'][data['trail'].length-1]['lng'];
-				flightsDetail[flight]['distance'] = geolib.getDistance({latitude: dagneuxGPS_lt, longitude: dagneuxGPS_lg}, {latitude: flightsDetail[flight]['latitude'], longitude: flightsDetail[flight]['longitude']})/1000+'km';
+
+		// Dès que toutes les infos de tous les avions sont récupérées, on passe à la suite
+		Promise.all(flightsPromises).then(function(flightsDetailResponse){
+			// On parcours chaque vol pour l'alimenter avec les nouvelles données reçues.
+			flightsDetailResponse.forEach(flightDetailResponse => {
+				let data = flightDetailResponse.data;
+				let flightId = data['identification']['id'];
+				flightsDetail[flightId] = flightsDetail[flightId] || {};
+				flightsDetail[flightId]['flight'] = data['identification']['callsign'] ? data['identification']['callsign'] : 'No callsign';
+				flightsDetail[flightId]['aircraft'] = data['aircraft'] && data['aircraft']['model'] && data['aircraft']['model']['text'] ? data['aircraft']['model']['text'] : '';
+				flightsDetail[flightId]['airline'] = data['airline'] && data['airline']['name'] ? data['airline']['name'] : '';
+				flightsDetail[flightId]['origin'] = data['airport'] && data['airport']['origin'] && data['airport']['origin'] && data['airport']['origin']['code'] && data['airport']['origin']['code']['iata'] ? data['airport']['origin']['code']['iata'] : '';
+				flightsDetail[flightId]['destination'] = data['airport'] && data['airport']['destination'] && data['airport']['destination'] && data['airport']['destination']['code'] && data['airport']['destination']['code']['iata'] ? data['airport']['destination']['code']['iata'] : '';
+
+				// On calcule avec geolib la distance qui sépare notre point gps (homeLatitude,homeLongitude) de notre avion. Pratique pour trouver l'avion au dessus de notre tête (= le plus près)
+				flightsDetail[flightId]['distance'] = geolib.getDistance({latitude: homeLatitude, longitude: homeLongitude}, {latitude: flightsDetail[flightId]['latitude'], longitude: flightsDetail[flightId]['longitude']});
+
+				// On ne connait pas toujours l'origine et/ou la destination des vols (pour quelques vols privés uniquement)
+				// On en profite pour récupérer avec "airports" le nom de la ville correspondant au code aéroport. Ex: "CDG" = "Paris", "LYS" = "Lyon"
+				if(flightsDetail[flightId]['origin']){
+					flightsDetail[flightId]['origin'] = airports.findWhere({ iata: flightsDetail[flightId]['origin'] }).get('city');
+				}
+
+				if(flightsDetail[flightId]['destination']){
+					flightsDetail[flightId]['destination'] = airports.findWhere({ iata: flightsDetail[flightId]['destination'] }).get('city');
+				}
 			})
-			
-			res.send(flightsDetail);
+
+			// Dernier traitement avant de renvoyer tout ça, on trie les vols par distance croissante, afin de récupérer en premier le vol au dessus de notre tête
+			let response = [];
+			for(let flight in flightsDetail){
+				response.push(flightsDetail[flight]);
+			}
+			response = response.sort((a,b) => (a.distance > b.distance) ? 1 : ((b.distance > a.distance) ? -1 : 0));
+
+			// That's all, on renvoie tout ça!
+			res.send(response);
 		});
-	
-	});
+
+	}).catch(error => {
+		console.log('err', error),
+		res.send({'error': error});
+ 	});
 });
 
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Listen port 3000');
 })
